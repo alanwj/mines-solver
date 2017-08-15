b41a8cd2ecee4d9e224716acbf1bfabea1998907
Alan Johnson <alan.wayne.johnson@gmail.com>
2017-08-15 15:38:53 -0700

    Move MinesWindow into its own file and rename GameWindow.

---
+++ mines/ui/builder_widget.h
@@ -0,0 +1,24 @@
+#ifndef MINES_UI_BUILDER_WIDGET_H_
+#define MINES_UI_BUILDER_WIDGET_H_
+
+namespace mines {
+namespace ui {
+
+// Gets the named widget from the builder.
+//
+// This is a convenience function to make calling get_widget_derived easier.
+//
+// The caller is responsible for correct memory management of the returned
+// Widget. See Gtk::Builder::get_widget_derived for details.
+template <typename T>
+T* GetBuilderWidget(const Glib::RefPtr<Gtk::Builder>& builder,
+                    const Glib::ustring& name) {
+  T* widget = nullptr;
+  builder->get_widget_derived(name, widget);
+  return widget;
+}
+
+}  // namespace ui
+}  // namespace mines
+
+#endif  // MINES_UI_BUILDER_WIDGET_H_
--- mines/ui/gtk_ui.cpp
+++ mines/ui/gtk_ui.cpp
@@ -28,7 +28,9 @@
 #include "mines/game/game.h"
 #include "mines/game/grid.h"
 #include "mines/solver/solver.h"
+#include "mines/ui/builder_widget.h"
 #include "mines/ui/elapsed_time_counter.h"
+#include "mines/ui/game_window.h"
 #include "mines/ui/mine_field.h"
 #include "mines/ui/remaining_mines_counter.h"
 #include "mines/ui/reset_button.h"
@@ -38,109 +40,6 @@ namespace mines {
 namespace ui {
 
 namespace {
-
-// Gets the named widget from the builder.
-//
-// This is a convenience function to make calling get_widget_derived easier.
-template <typename T>
-T* GetBuilderWidget(const Glib::RefPtr<Gtk::Builder>& builder,
-                    const Glib::ustring& name) {
-  T* widget = nullptr;
-  builder->get_widget_derived(name, widget);
-  return widget;
-}
-
-// The main window for the game.
-class MinesWindow : public Gtk::ApplicationWindow {
- public:
-  MinesWindow(BaseObjectType* cobj, const Glib::RefPtr<Gtk::Builder>& builder)
-      : Gtk::ApplicationWindow(cobj),
-        field_(GetBuilderWidget<MineField>(builder, "mine-field")),
-        reset_button_(GetBuilderWidget<ResetButton>(builder, "reset-button")),
-        remaining_mines_counter_(GetBuilderWidget<RemainingMinesCounter>(
-            builder, "remaining-mines-counter")),
-        elapsed_time_counter_(GetBuilderWidget<ElapsedTimeCounter>(
-            builder, "elapsed-time-counter")),
-        solver_algorithm_(solver::Algorithm::NONE) {
-    add_action("new", sigc::mem_fun(this, &MinesWindow::NewGame));
-    solver_action_ = add_action_radio_string(
-        "solver", sigc::mem_fun(this, &MinesWindow::NewSolverAlgorithm),
-        "none");
-
-    field_->signal_action().connect(
-        sigc::mem_fun(this, &MinesWindow::HandleAction));
-
-    reset_button_->signal_clicked().connect(
-        sigc::mem_fun(this, &MinesWindow::NewGame));
-    reset_button_->ConnectToMineField(*field_);
-
-    NewGame();
-  }
-
- private:
-  // Starts a new game.
-  void NewGame() {
-    game_ = mines::NewGame(16, 30, 99, std::time(nullptr));
-    solver_ = solver::New(solver_algorithm_, *game_);
-    field_->Reset(*game_);
-    reset_button_->Reset(*game_);
-    remaining_mines_counter_->Reset(*game_);
-    elapsed_time_counter_->Reset(*game_);
-  }
-
-  // Changes the solver algorithm and starts a new game.
-  void NewSolverAlgorithm(const Glib::ustring& target) {
-    solver_action_->change_state(target);
-
-    if (target == "none") {
-      solver_algorithm_ = solver::Algorithm::NONE;
-    } else if (target == "local") {
-      solver_algorithm_ = solver::Algorithm::LOCAL;
-    } else {
-      solver_algorithm_ = solver::Algorithm::NONE;
-    }
-
-    NewGame();
-  }
-
-  // Handles an action on the mine field.
-  //
-  // Executes the action, updates the mine field, and runs the solver.
-  void HandleAction(Action action) {
-    game_->Execute(action);
-
-    // Execute all actions recommended by the solver.
-    std::vector<Action> actions;
-    do {
-      actions = solver_->Analyze();
-      game_->Execute(actions);
-    } while (!actions.empty());
-  }
-
-  // The mine field.
-  MineField* field_;
-
-  // The reset button.
-  ResetButton* reset_button_;
-
-  // The remaining mines counter.
-  RemainingMinesCounter* remaining_mines_counter_;
-
-  // The elapsed time counter.
-  ElapsedTimeCounter* elapsed_time_counter_;
-
-  // Menu action for selection the solver algorithm.
-  Glib::RefPtr<Gio::SimpleAction> solver_action_;
-
-  // The algorithm used in current and new games.
-  solver::Algorithm solver_algorithm_;
-
-  // The current game.
-  std::unique_ptr<Game> game_;
-
-  // The current solver.
-  std::unique_ptr<solver::Solver> solver_;
-};
 
 // The master GTK application.
 class MinesApplication : public Gtk::Application {
@@ -166,7 +65,7 @@ class MinesApplication : public Gtk::Application {
   void on_activate() final {
     Gtk::Application::on_activate();
 
-    window_.reset(GetBuilderWidget<MinesWindow>(builder_, "app-window"));
+    window_.reset(GetBuilderWidget<GameWindow>(builder_, "app-window"));
     add_window(*window_);
     window_->signal_hide().connect(
         sigc::mem_fun(this, &MinesApplication::OnHideWindow));
@@ -177,7 +76,7 @@ class MinesApplication : public Gtk::Application {
   void OnHideWindow() { window_.reset(); }
 
   Glib::RefPtr<Gtk::Builder> builder_;
-  std::unique_ptr<MinesWindow> window_;
+  std::unique_ptr<GameWindow> window_;
 };
 
 }  // namespace
