2d17d6dcaf0934a51795c9f74c8ddd8cc1110180
Alan Johnson <alan.wayne.johnson@gmail.com>
2017-08-27 02:08:14 -0700

    New region analysis attempt.

--- mines/solver/constraint_analyzer.cpp
+++ mines/solver/constraint_analyzer.cpp
@@ -1,64 +1,113 @@
 #include "mines/solver/constraint_analyzer.h"
 
-#include <map>
-#include <set>
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
 
 #include "mines/game/grid.h"
+
+namespace std {
+
+template <>
+struct hash<mines::solver::CellLocation> {
+  // This is 2^64 / phi. Any irrational number would do. The goal is just
+  // "random" bits.
+  static constexpr std::size_t magic = 0x9e3779b97f4a7a97;
+
+  std::size_t operator()(const mines::solver::CellLocation& location) const {
+    const std::hash<std::size_t> h;
+    std::size_t seed = h(location.row);
+    seed ^= h(location.col) + magic + (seed << 6) + (seed >> 2);
+    return seed;
+  }
+};
+
+}  // namespace std
 
 namespace mines {
 namespace solver {
 
 namespace {
 
+class CellLocationMap {
+ public:
+  explicit CellLocationMap(std::size_t bucket_count)
+      : index_map_(bucket_count) {
+    locations_.reserve(bucket_count);
+  }
+
+  std::size_t operator[](const CellLocation& location) {
+    auto result =
+        index_map_.insert(std::make_pair(location, locations_.size()));
+    if (result.second) {
+      locations_.push_back(location);
+    }
+    return result.first->second;
+  }
+
+  std::vector<CellLocation>&& MoveLocations() { return std::move(locations_); }
+
+ private:
+  std::unordered_map<CellLocation, std::size_t> index_map_;
+  std::vector<CellLocation> locations_;
+};
+
 class ConstraintAnalyzerImpl : public ConstraintAnalyzer {
  public:
   ConstraintAnalyzerImpl(std::size_t rows, std::size_t cols)
       : grid_(rows, cols) {}
 
-  std::vector<Constraint> AnalyzeConstraints() final {
+  ~ConstraintAnalyzerImpl() final = default;
+
+  ConstraintAnalysis Analyze() final {
+    // Make all the constraints.
+    CellLocationMap location_map(8 * cell_locations_.size());
     std::vector<Constraint> constraints;
-    for (auto it = cell_loc_.begin(); it != cell_loc_.end();) {
+    constraints.reserve(cell_locations_.size());
+    for (auto it = cell_locations_.begin(); it != cell_locations_.end();) {
       auto cur = it++;
-      Constraint constraint{MakeConstraint(*cur)};
+      Constraint constraint{MakeConstraint(*cur, location_map)};
       if (constraint.IsStable()) {
         // The constraint has become stable, so we may remove it from analysis.
-        cell_loc_.erase(cur);
+        cell_locations_.erase(cur);
       } else {
         constraints.push_back(std::move(constraint));
       }
     }
-    return constraints;
-  }
+    std::vector<CellLocation> locations{location_map.MoveLocations()};
 
-  std::vector<Region> AnalyzeRegions(
-      std::vector<Constraint>& constraints) final {
+    // Union all of the locations in each constraint.
+    // This builds the disjoint set tree in place within the grid.
     for (const Constraint& constraint : constraints) {
-      const std::vector<CellLocation>& locations = constraint.GetLocations();
-      if (locations.empty()) {
+      const auto& location_indexes = constraint.GetLocationIndexes();
+      if (location_indexes.empty()) {
         continue;
       }
-      auto first = locations.begin();
-      for (auto it = first + 1; it != locations.end(); ++it) {
-        Union(*first, *it);
+      auto first = location_indexes.begin();
+      const CellLocation& first_location = locations[*first];
+      for (auto it = first + 1; it != location_indexes.end(); ++it) {
+        Union(first_location, locations[*it]);
       }
     }
 
-    std::map<CellLocation, std::vector<const Constraint*>> constraint_map;
+    // Map the disjoint set tree into explicit sets.
+    std::unordered_map<CellLocation, std::vector<Constraint>> constraint_map;
     for (Constraint& constraint : constraints) {
-      const std::vector<CellLocation>& locations = constraint.GetLocations();
-      if (locations.empty()) {
+      const auto& location_indexes = constraint.GetLocationIndexes();
+      if (location_indexes.empty()) {
         continue;
       }
-      CellLocation loc = Find(locations[0]);
-      constraint_map[loc].push_back(&constraint);
+      CellLocation location = Find(locations[location_indexes[0]]);
+      constraint_map[location].push_back(std::move(constraint));
     }
 
+    // Create regions from the explicit sets.
     std::vector<Region> regions;
     for (auto& p : constraint_map) {
       regions.push_back(Region(std::move(p.second)));
     }
 
-    return regions;
+    return ConstraintAnalysis(std::move(locations), std::move(regions));
   }
 
   void NotifyEvent(const Event& event) final {
@@ -71,7 +120,7 @@ class ConstraintAnalyzerImpl : public ConstraintAnalyzer {
       case Event::Type::UNCOVER:
         cell.state = CellState::UNCOVERED;
         if (event.adjacent_mines != 0) {
-          cell_loc_.insert({event.row, event.col});
+          cell_locations_.insert({event.row, event.col});
         }
         break;
       case Event::Type::FLAG:
@@ -83,7 +132,7 @@ class ConstraintAnalyzerImpl : public ConstraintAnalyzer {
                                                            std::size_t col) {
           const Cell& cell = grid_(row, col);
           if (cell.state == CellState::UNCOVERED && cell.adjacent_mines != 0) {
-            cell_loc_.insert({row, col});
+            cell_locations_.insert({row, col});
           }
           return false;
         });
@@ -112,9 +161,9 @@ class ConstraintAnalyzerImpl : public ConstraintAnalyzer {
     std::size_t ds_rank = 0;
   };
 
-  CellLocation Find(const CellLocation& loc) {
-    Cell& cell = grid_(loc.row, loc.col);
-    if (cell.ds_parent != loc) {
+  CellLocation Find(const CellLocation& location) {
+    Cell& cell = grid_(location.row, location.col);
+    if (cell.ds_parent != location) {
       cell.ds_parent = Find(cell.ds_parent);
     }
     return cell.ds_parent;
@@ -143,34 +192,35 @@ class ConstraintAnalyzerImpl : public ConstraintAnalyzer {
     }
   }
 
-  Constraint MakeConstraint(const CellLocation& loc) {
-    const Cell& cell = grid_(loc.row, loc.col);
+  Constraint MakeConstraint(const CellLocation& location,
+                            CellLocationMap& location_map) {
+    const Cell& cell = grid_(location.row, location.col);
 
-    std::vector<CellLocation> locs;
-    locs.reserve(cell.adjacent_mines);
+    std::vector<std::size_t> location_indexes;
+    location_indexes.reserve(cell.adjacent_mines);
     std::size_t flags = 0;
 
-    grid_.ForEachAdjacent(
-        loc.row, loc.col,
-        [this, &locs, &flags](std::size_t row, std::size_t col) {
-          Cell& cell = grid_(row, col);
-          if (cell.state == CellState::COVERED) {
-            locs.push_back({row, col});
-            cell.ds_parent = CellLocation{row, col};
-            cell.ds_rank = 0;
-          } else if (cell.state == CellState::FLAGGED) {
-            ++flags;
-          }
-          return false;
-        });
+    auto f = [this, &location_map, &location_indexes, &flags](std::size_t row,
+                                                              std::size_t col) {
+      Cell& cell = grid_(row, col);
+      if (cell.state == CellState::COVERED) {
+        location_indexes.push_back(location_map[{row, col}]);
+        cell.ds_parent = CellLocation{row, col};
+        cell.ds_rank = 0;
+      } else if (cell.state == CellState::FLAGGED) {
+        ++flags;
+      }
+      return false;
+    };
+    grid_.ForEachAdjacent(location.row, location.col, f);
 
     std::size_t mines =
         cell.adjacent_mines <= flags ? cell.adjacent_mines - flags : 0;
 
-    return Constraint(std::move(locs), mines);
+    return Constraint(std::move(location_indexes), mines);
   }
 
-  std::set<CellLocation> cell_loc_;
+  std::unordered_set<CellLocation> cell_locations_;
   Grid<Cell> grid_;
 };
 
--- mines/solver/constraint_analyzer.h
+++ mines/solver/constraint_analyzer.h
@@ -16,10 +16,6 @@ struct CellLocation {
   std::size_t col;
 };
 
-inline bool operator<(const CellLocation& a, const CellLocation& b) {
-  return a.row < b.row || (a.row == b.row && a.col < b.col);
-}
-
 inline bool operator==(const CellLocation& a, const CellLocation& b) {
   return a.row == b.row && a.col == b.col;
 }
@@ -32,16 +28,8 @@ inline bool operator!=(const CellLocation& a, const CellLocation& b) {
 // cells and the number of mines that must be in those cells.
 class Constraint {
  public:
-  Constraint(std::vector<CellLocation> locs, std::size_t mines)
-      : locs_(std::move(locs)), mines_(mines) {}
-
-  ~Constraint() = default;
-
-  Constraint(const Constraint&) = delete;
-  Constraint& operator=(const Constraint&) = delete;
-
-  Constraint(Constraint&&) = default;
-  Constraint& operator=(Constraint&&) = default;
+  Constraint(std::vector<std::size_t> location_indexes, std::size_t mines)
+      : location_indexes_(std::move(location_indexes)), mines_(mines) {}
 
   // A constraint is considered stable if it can have no more effect on the
   // game.
@@ -49,51 +37,59 @@ class Constraint {
   // This occurs when all adjacent cells are uncovered except those that are
   // flagged. The only action that can change a stable constraint is unflagging
   // a previously flagged cell.
-  bool IsStable() const { return mines_ == 0 && locs_.empty(); }
+  bool IsStable() const { return mines_ == 0 && location_indexes_.empty(); }
 
   // Returns the locations affected by the constraint.
-  const std::vector<CellLocation>& GetLocations() const { return locs_; }
+  //
+  // These are indexes into the constraint analysis location vector.
+  const std::vector<std::size_t>& GetLocationIndexes() const {
+    return location_indexes_;
+  }
 
   // Returns the number of mines in the locations affected by this cell.
   std::size_t GetMines() const { return mines_; }
 
  private:
-  std::vector<CellLocation> locs_;
+  std::vector<std::size_t> location_indexes_;
   std::size_t mines_;
 };
 
-// Represesnts a set of constraints that are disjoint in effect. Constraints in
+// Represents a set of constraints that are disjoint in effect. Constraints in
 // one region may be logically analyzed separately from other regions.
-//
-// Note: Regions only contain pointers to constraints, which much continue to
-// exist for the lifetime of the region.
 class Region {
  public:
-  explicit Region(std::vector<const Constraint*> constraints)
+  explicit Region(std::vector<Constraint> constraints)
       : constraints_(std::move(constraints)) {}
 
-  ~Region() = default;
-
-  Region(const Region&) = delete;
-  Region& operator=(const Region&) = delete;
-
-  Region(Region&&) = default;
-  Region& operator=(Region&&) = default;
-
-  std::vector<const Constraint*> GetConstraints() const { return constraints_; }
+  // Returns the constraints in this region.
+  const std::vector<Constraint>& GetConstraints() const { return constraints_; }
 
  public:
-  std::vector<const Constraint*> constraints_;
+  std::vector<Constraint> constraints_;
+};
+
+class ConstraintAnalysis {
+ public:
+  ConstraintAnalysis(std::vector<CellLocation> locations,
+                     std::vector<Region> regions)
+      : locations_(std::move(locations)), regions_(std::move(regions)) {}
+
+  const std::vector<CellLocation>& GetLocations() const { return locations_; }
+  const std::vector<Region>& GetRegions() const { return regions_; }
+
+ private:
+  std::vector<CellLocation> locations_;
+  std::vector<Region> regions_;
 };
 
 class ConstraintAnalyzer : public EventSubscriber {
  public:
-  virtual std::vector<Constraint> AnalyzeConstraints() = 0;
+  virtual ~ConstraintAnalyzer() = default;
 
-  virtual std::vector<Region> AnalyzeRegions(
-      std::vector<Constraint>& constraints) = 0;
+  virtual ConstraintAnalysis Analyze() = 0;
 };
 
+// Returns a new ConstraintAnalyzer.
 std::unique_ptr<ConstraintAnalyzer> NewConstrantAnalyzer(Game& game);
 
 }  // namespace solver
